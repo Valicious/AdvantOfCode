2f62e786e786274e611425bc85281c4d3f531f0a
Gerhard <gert.pe.agg@gmail.com>
2018-12-01 22:14:04 +0200

    Done Day 1 part 1 and 2

---
+++ src/d1p1.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class d1p1 {
+    private static int tally;
+
+    public static void main(String[] args) {
+        System.out.println("Starting! Day 1, Part 1");
+        System.out.println("Reading loading file!!");
+        List<String> frequencies = populateList("rsc/d1p1");
+        if (frequencies.size() != 0) {
+            System.out.println("Adding up frequencies");
+            tally = 0;
+            frequencies.forEach(d1p1::tallyIt);
+        }
+        System.out.println("Frequency is " + tally);
+        System.out.println("Program done!");
+    }
+
+    public static void tallyIt(String value) {
+        int val = Integer.parseInt(value.substring(1));
+        if (value.charAt(0) == 45)
+            tally -= val;
+        else
+            tally += val;
+    }
+
+    public static List<String> populateList(String strFile) {
+        File file = new File(strFile);
+        List<String> frequencies = new ArrayList<>();
+        try {
+            Scanner scan = new Scanner(file);
+            while (scan.hasNext())
+                frequencies.add(scan.nextLine());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return frequencies;
+    }
+
+}
---
+++ src/d1p2.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+public class d1p2 {
+    private static int tally;
+    private static Set<Integer> uniqFreq;
+    private static boolean dupFound;
+    private static int firstFinalFreq;
+
+    public static void main(String[] args) {
+        System.out.println("Starting! Day 1, Part 2");
+        System.out.println("Loading file!");
+        List<String> frequencies = d1p1.populateList("rsc/d1p1");
+        if (frequencies.size() != 0) {
+            firstFinalFreq = -1;
+            dupFound = false;
+            System.out.println("Adding up frequencies");
+            tally = 0;
+            uniqFreq = new HashSet<>();
+            while (!dupFound) {
+                frequencies.forEach(d1p2::tallyIt);
+                firstFinalFreq = firstFinalFreq == -1 ? tally : firstFinalFreq;
+            }
+        }
+        System.out.println("First Frequency is " + firstFinalFreq);
+       // System.out.println("Final Frequency after duplicate is found " + tally);
+        System.out.println("Program done!");
+    }
+
+    public static void tallyIt(String value) {
+        int val = Integer.parseInt(value.substring(1));
+        if (value.charAt(0) == 45)
+            tally -= val;
+        else
+            tally += val;
+        if (uniqFreq.contains(tally)) {
+            if (dupFound) return;
+            System.out.println("Duplicate value " + tally);
+            dupFound = true;
+        } else
+            uniqFreq.add(tally);
+    }
+}
